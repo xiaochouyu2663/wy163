414ae98fca54c9d3c9790574f8dd2f3cf9f82aad
sherry <weixiaoli0210@163.com>
2017-12-21 18:08:06 +0800

    vue-scroller

--- qishi/src/components/Home.vue
+++ qishi/src/components/Home.vue
@@ -1,29 +1,33 @@
 <template>
   <div id="apper">
-    <view-box ref="viewBoxs" id="wk_list">
+    <view-box ref="viewBox" >
       <x-header @on-click-title="getscroll()" class="header" style="" slot="header" :left-options="{showBack:false,backText:''}">
         <div>康妆商城</div>
       </x-header>
-      <scroller class="my_scroller" :on-infinite="infinite">
-        <swiper :list="swiperList" auto loop dots-position="right">
-          
-        </swiper>
-        <tab class="tabnar">
-          <tab-item selected>全部</tab-item>
-          <tab-item>小白系列</tab-item>
-          <tab-item>授权系列</tab-item>
-        </tab>
-        <div class="product_list" >
-          <div class="product_list_item" v-for="item in productList" >
-            <a :href="'detail.html?proid='+item.product_id">
-              <img :src="item.product_image" alt="">
-              <p>{{item.product_name}}</p>
-              <span>{{item.product_price}}</span>
-            </a>
+      
+        <div slot="default">
+          <scroller class="my_scroller"  ref="scroller" :onRefresh="refresh" refreshText="loading..." :onInfinite="infinit">
+          <swiper :list="swiperList" auto loop dots-position="right">
             
+          </swiper>
+          <tab class="tabnar">
+            <tab-item selected>全部</tab-item>
+            <tab-item>小白系列</tab-item>
+            <tab-item>授权系列</tab-item>
+          </tab>
+          <div class="product_list" >
+            <div class="product_list_item" v-for="item in productList" >
+              <a :href="'detail.html?proid='+item.product_id">
+                <img :src="item.product_image" alt="">
+                <p>{{item.product_name}}</p>
+                <span>{{item.product_price}}</span>
+              </a>
+              
+            </div>
           </div>
+          </scroller>
         </div>
-      </scroller>
+      
       
     </view-box>
   </div>
@@ -43,13 +47,51 @@ export default {
     return {
       // swiperList:[{ url: 'javascript:', img: 'https://static.vux.li/demo/1.jpg', title: '送你一朵fua' }, { url: 'javascript:', img: 'https://static.vux.li/demo/5.jpg', title: '送你一次旅行', fallbackImg: 'https://static.vux.li/demo/3.jpg' }],
       swiperList:[],
-      productList:[]
+      productList:[],
+      sellerList:[]
     }
   },
   methods:{
-    
+    infinit(done){
+
+     var self=this;
+        setTimeout(()=>{
+          console.log('下拉加载')
+          axios.get('http://localhost/tp5/public/index.php/index/index/getBanner',{
+            params:{}
+          })
+          .then(res=>{
+            self.productList=self.productList.concat(res.data.map(item=>{
+                return {
+                  product_image:'http://localhost/tp5/public/static/'+item.product_image,
+                  product_name:item.product_name,
+                  product_price:item.product_price,
+                  product_id:item.product_id
+                }
+              })
+            )
+          })
+          .catch(res=>{
+            console.log(res)
+          })
+          
+           setTimeout(() => {
+            done()
+          })
+        },1500)
+    },
+    refresh(done){
+      var self=this;
+      setTimeout(()=>{
+        console.log('下拉刷新')
+        // self.$refs.scroller.triggerPullToRefresh();  手动刷新
+        done()
+      },1000)
+      
+    }
   },
   created(){
+        console.log('created')
         //获取banner数据 
        axios.get('http://localhost/tp5/public/index.php/index/index/getBanner',{
          params:{}
@@ -64,30 +106,60 @@ export default {
            }
          })
          this.productList=res.data.map(item=>{
-           return {
-            product_image:'http://localhost/tp5/public/static/'+item.product_image,
-            product_name:item.product_name,
-            product_price:item.product_price,
-            product_id:item.product_id
-           }
-         })
+            return {
+              product_image:'http://localhost/tp5/public/static/'+item.product_image,
+              product_name:item.product_name,
+              product_price:item.product_price,
+              product_id:item.product_id
+            }
+          })
          
        })
        .catch(res=>{
          console.log(res)
        })
   },
+  beforeCreate(){
+    console.log('beforeCreate')
+  },
+  beforeMount(){
+    console.log('beforeMount')
+    console.log(this)
+  },
   mounted(){
+    // console.log('mounted')
+    // console.log(this.$refs.scroller.getPosition())   //获取scroller当前的位置
+    // setTimeout(() => {
+    //   console.log(this.$refs.scroller.getPosition())
+    //   //this.$refs.scroller.scrollTo(0,1000,true);      //指定scroller到当前的位置
+    //   this.$refs.scroller.scrollBy(0,1000,true)         //指定scroller到当前的位置
+
+
+    //   this.$refs.scroller.finishPullToRefresh()
+    // }, 5000);
     
-    this.$nextTick(function () {
-    var oScroDom=this.$refs.viewBoxs.getScrollBody();
-      console.log(oScroDom)
-      console.log(this.$refs.viewBoxs.scrollTo)
-      this.$refs.viewBoxs.scrollTo(0,'34px');
-      console.log(this.$refs.viewBoxs.getScrollTop())
-  })
-    
-  }
+
+  },
+
+  beforeRouteEnter(to,from,next){
+    console.log(sessionStorage.askPositon)
+    if(!sessionStorage.askPositon || from.path == '/'){//当前页面刷新不需要切换位置
+      sessionStorage.askPositon = '';
+      next();
+    }else{
+      next(vm => {
+          if(vm && vm.$refs.scrollerBottom){//通过vm实例访问this
+            setTimeout(function () {
+              vm.$refs.scrollerBottom.scrollTo(0, sessionStorage.askPositon, false);
+            },0)//同步转异步操作
+          }
+      })
+    }
+  },
+beforeRouteLeave(to,from,next){//记录离开时的位置
+  sessionStorage.askPositon = this.$refs.scrollerBottom && this.$refs.scrollerBottom.getPosition() && this.$refs.scrollerBottom.getPosition().top;
+  next()
+},
 }
 </script>
 
@@ -106,8 +178,11 @@ export default {
   #apper{
     height:100%;
     .my_scroller{
-    top:46px;
-  }
+      top:46px;
+      height: auto;
+      padding-bottom:100px;
+      margin-bottom:60px;
+    }
   }
   .header{
     background-color: #4db90a!important;
@@ -125,9 +200,7 @@ export default {
   .tabnar{
     margin-top:15px;
   }
-  .my_scroller{
-    padding-bottom:50px;
-  }
+  
   .product_list_item{
     background:#fff;
     margin-bottom:10px;
--- qishi/src/components/Person.vue
+++ qishi/src/components/Person.vue
@@ -1,9 +1,34 @@
 <template>
-  <div>sdfs</div>
+  <view-box ref="viewBox">
+    <div v-for="i in 99">
+      {{i}}
+    </div>
+  </view-box>
 </template>
 <script>
+import {ViewBox} from 'vux';
 export default {
-  
+  name:'Person',
+  components:{
+    ViewBox
+  },
+  data(){
+    return {
+
+    }
+  },
+ mounted(){
+    
+    this.$nextTick(function () {
+        var oScroDom=this.$refs.viewBox.getScrollBody();  //获取滚动div, 也可以直接用组件引用的.$refs.viewBoxBody	
+        var that=this;
+        this.$refs.viewBox.scrollTo(200);   //指定viewbox滚动到指定的位置
+        oScroDom.addEventListener('scroll',function(){
+          console.log(that.$refs.viewBox.getScrollTop())   //获取viewbox当前的滚动位置
+        },false)
+      
+  })
+ }
 }
 </script>
 <style>
