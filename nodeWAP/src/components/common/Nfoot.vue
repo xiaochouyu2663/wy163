70faa23c049b9d7afa9db47ca3f33f7b8792e892
sherry <weixiaoli0210@163.com>
2018-02-09 18:07:00 +0800

    增加tabbar

---
+++ nodeWAP/src/components/common/Nfoot.vue
@@ -0,0 +1,34 @@
+<template>
+    <mt-tabbar >
+      <mt-tab-item id="首页">
+         <img slot="icon" src="../../assets/images/icon_home_default.png">
+        首页
+      </mt-tab-item>
+      <mt-tab-item id="分类">
+        <img slot="icon" src="../../assets/images/icon_searchcate_default.png">
+        分类
+      </mt-tab-item>
+      <mt-tab-item id="购物车">
+        <img slot="icon" src="../../assets/images/icon_cart_default.png">
+        购物车
+      </mt-tab-item>
+      <mt-tab-item id="个人中心">
+        <img slot="icon" src="../../assets/images/icon_member_default.png">
+        个人中心
+      </mt-tab-item>
+    </mt-tabbar>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
